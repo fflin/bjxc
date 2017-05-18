fac4ead6818a24d000743f502bcbd54e86c94cbe
hw <790171314@qq.com>
2017-05-18 09:22:25 +0800

    修改baseurl

--- app/src/main/java/com/zxwl/frame/net/Urls.java
+++ app/src/main/java/com/zxwl/frame/net/Urls.java
@@ -8,7 +8,7 @@ package com.zxwl.frame.net;
 
 public class Urls {
 //    public static final String BASE_URL = "http://192.168.222.137/hbsw/";
-    public static final String BASE_URL = "http://192.168.0.122:8080/bjxc/";
+    public static final String BASE_URL = "http://192.168.0.111:8080/bjxc/";
 //    public static final String BASE_URL = "http://192.168.222.233:10119/bjxc/";
 
     /**
